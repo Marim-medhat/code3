0667ec0a520e517864e97839c083cca2d23b26af
Marim-medhat <36182751+Marim-medhat@users.noreply.github.com>
2018-03-27 04:54:42 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+void sortnum(int&int&,int&)
+int x,y,z;
+    cout<<"enter nums';
+    cin >>x>>y>>z;
+    int max=0
+        if(max<x){max=x;}
+        if(max<y){max=y;}
+        if(max<z){max=z;}
+        max=x
+         int man=0
+        if(man<y){man=y;}
+        if(man<z){man=z;}
+        man=y
+
+
+
+int main()
+{
+
+
+    return 0;
+}
